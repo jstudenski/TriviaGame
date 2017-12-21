7b5b62ab7e419192361352897f7411b988d791ce
jstudenski <jstudenski@gmail.com>
2017-12-21 14:30:40 -0600

    added gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,44 +15,53 @@ var questions = [
   {
     "question":"Who created of the now-famous chopped and screwed DJ technique?",
     "guesses":["Dj Khaled","DJ Green Lantern","DJ Screw","Dj Drama"],
-    "answer":"DJ Screw"
+    "answer":"DJ Screw",
+    "image":"djskrew.gif"
   },
   {
     "question":"Who was the top selling artist of the 2000's?",
     "guesses":["Lil Wayne","Eminem","Jay Z","Kanye West"],
-    "answer":"Eminem"
+    "answer":"Eminem",
+    "image":"eminem.gif"
   },
   {
     "question":"Which member of UGK passed away in 2007?",
     "guesses":["Pimp C","Bun B","Andre 3000","Slim Thug"],
-    "answer":"Pimp C"
+    "answer":"Pimp C",
+    "image":"pimpc.gif"
   },
   {
     "question":"Who is the female member in the goup Three 6 Mafia?",
     "guesses":["Gangsta Boo","Lil' Kim","Missy Elliott","Lauryn Hill"],
-    "answer":"Gangsta Boo"
+    "answer":"Gangsta Boo",
+    "image":"gangstaboo.gif"
   },
   {
     "question":"What was rapper 2 Chainz formerly known as?",
     "guesses":["2 Pistols","Dj Kool","Big Dawg","Tity Boi"],
-    "answer":"Tity Boi"
+    "answer":"Tity Boi",
+    "image":"chainz.gif"
   },
   {
-    "question":"During a 10-day suspension from highschool, who recorded their first mixtape titled 10 Day",
+    "question":"During a 10-day suspension from highschool, who recorded their first mixtape titled 10 Day?",
     "guesses":["Snoop Dogg","Chance the Rapper","Childish Gambino","Vic Mensa"],
-    "answer":"Chance the Rapper"
+    "answer":"Chance the Rapper",
+    "image":"chance.gif"
   },
   {
-    "question":"",
-    "guesses":["","","",""],
-    "answer":""
+    "question":"In 2017, who produced 10% of all songs on the bilboard top 40?",
+    "guesses":["Zaytoven","Sonny Digital","Metro Boomin","Dj Mustard"],
+    "answer":"Metro Boomin",
+    "image":"metroboomin.gif"
   },
   {
-    "question":"",
-    "guesses":["","","",""],
-    "answer":""
+    "question":"Who was only 16 when he recorded his breakout single No Flockin'?",
+    "guesses":["Kodak Black","21 Savage","Young Thug","Playboi Carti"],
+    "answer":"Kodak Black",
+    "image":"kodak.gif"
   }
 ];
+
 
 // Timer stuff
 var number;
@@ -132,6 +141,14 @@ function displayResult(status){
     $("#status").append('<br>The correct answer was: ' + questions[currentQ].answer);
     
   }
+
+  $('<img/>')
+    .attr('src', "assets/images/" + questions[currentQ].image + "")
+    .attr('alt', questions[currentQ].answer)
+    .width('250px')
+    .appendTo($('#status'));
+  
+
  
   setTimeout(function(){
     $("#status").html('');
