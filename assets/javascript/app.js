6ffc322ac55d97ae54b01531168fa3e8badf8c93
jstudenski <jstudenski@gmail.com>
2017-12-21 18:17:49 -0600

    final push

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,10 @@ var correct = 0;
 var incorrect = 0;
 var incomplete = 0;
 
+// Timer stuff
+var number;
+var intervalId;
+var currentQ = 0;
 
 
 var questions = [
@@ -63,35 +67,25 @@ var questions = [
 ];
 
 
-// Timer stuff
-var number;
-var intervalId;
-var currentQ = 0;
-
 function newQuestion(q) {
-
   if (startGame === false) {
     $("#time-remaining").hide();
     makeStartBtn();
   } else {
     $("#time-remaining").show();
-
     startTimer();
 
     $("#question").html(questions[q].question);
-
-    var btn;
+    
     for(i=0; i < questions[q].guesses.length; i++){
-      btn = $("<button>")
-        .attr("data-guess", questions[q].guesses[i])
+      var btn = $("<button>");
+      btn.attr("data-guess", questions[q].guesses[i])
         .text(questions[q].guesses[i])
         .click(checkGuess);
       $("#guesses").append(btn);
     }
   }
 }
-
-
 
 function makeStartBtn(){
   var btn = $("<button>")
@@ -107,14 +101,11 @@ function newGame() {
   currentQ = 0;
   $(this).hide();
   newQuestion(currentQ);
-};
-
-
-function checkGuess() {
-
-  ($(this).attr("data-guess") === questions[currentQ].answer) ? displayResult('correct') : displayResult('incorrect');
 }
 
+function checkGuess() {
+  ($(this).attr("data-guess") === questions[currentQ].answer) ? displayResult('correct') : displayResult('incorrect');
+}
 
 function displayResult(status){
   clearInterval(intervalId);
@@ -124,17 +115,17 @@ function displayResult(status){
 
   if (status === 'correct') {
     correct++;
-    $("#status").html('Correct');
+    $("#status").html('<br>Correct<br>');
 
   } else if (status === 'incorrect') {
     incorrect++;
-    $("#status").html('Nope!');
-    $("#status").append('<br>The correct answer was: ' + questions[currentQ].answer);
+    $("#status").html('<br>Nope!<br>')
+      .append('<br>The correct answer was: ' + questions[currentQ].answer);
 
   } else if (status === 'timeup') {
     incomplete++;
-    $("#status").html('Out of Time!');
-    $("#status").append('<br>The correct answer was: ' + questions[currentQ].answer);
+    $("#status").html('Out of Time!')
+      .append('<br>The correct answer was: ' + questions[currentQ].answer);
   }
 
   $('<img/>')
@@ -149,36 +140,31 @@ function displayResult(status){
     if (currentQ < questions.length-1) {
       newQuestion(++currentQ);
     } else {
-      $("#status").html('Game is Over..')
-        .append('<br>Correct: ' + correct)
-        .append('<br>Incorrect: ' + incorrect)
+      $("#status").html('<br>Game is Over..<br>')
+        .append('<br>Correct: ' + correct +'<br>')
+        .append('<br>Incorrect: ' + incorrect+'<br>')
         .append('<br>Incomplete: ' + incomplete);
       makeStartBtn();
     }
 
 
-  }, 3000);
+  }, 3000); //3000
 
 }
 
-
 function startTimer() {
- number = 10; 
+ number = 30; 
  decrement();
  intervalId = setInterval(decrement, 1000);
 }
 
 function decrement() {
-  console.log(number);
-  $("#countdown").html(number);
-  number--
-
+  //console.log(number);
+  $("#countdown").html(number--);
   if (number < 0) {
     clearInterval(intervalId);
     displayResult('timeup');
   }
-
-
 }
 
 newQuestion();
